a91c3dccb43adab7647f4c876849c8c3ca47ae86
Anthony Chi <a35931chi@gmail.com>
2018-01-09 14:29:43 -0500

    made adjustments prior to submission

--- my_answers.py
+++ my_answers.py
@@ -32,23 +32,22 @@ def window_transform_series(series, window_size):
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size): #so this just outputs a model
     model = Sequential()
-    model.add(LSTM(128, input_shape = (None, window_size), return_sequences = True))
-    model.add(LSTM(128, return_sequences = False))
+    model.add(LSTM(5, input_shape = (window_size, 1))) #whynot (None, window_size)
     model.add(Dense(1))
     return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    from string import punctuation
+    from string import ascii_letters
     punctuation_keep = ['!', ',', '.', ':', ';', '?']
     keep_text = ''
     for char in text:
-        if char not in punctuation or char in punctuation_keep:
+        if char in ascii_letters or char in punctuation_keep:
             keep_text += char.lower()
         else:
             keep_text += ' '
-        
+    text = text.replace('  ',' ')
     return keep_text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -64,7 +63,7 @@ def window_transform_text(text, window_size, step_size):
         #print(idx-window_size,':',idx)
         #print(text[idx-window_size:idx], '-', text[idx])
 
-        idx += 5
+        idx += step_size
 
     return inputs, outputs
 
@@ -74,6 +73,7 @@ def build_part2_RNN(window_size, num_chars):
     model = Sequential()
     model.add(LSTM(200, input_shape = (window_size, num_chars)))
     model.add(Dense(num_chars))
-    model.add(Dense(activation = 'softmax', units = num_chars))
+    #model.add(Dense(activation = 'softmax', units = num_chars))
+    model.add(Activation('softmax'))
 
     return model
